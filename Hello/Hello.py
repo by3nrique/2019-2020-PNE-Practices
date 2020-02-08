2c28ac8d89358759faa6b8046d05dbee10d3c1ae
Enrique <e.feito.2019@alumnos.urjc.es>
2020-02-08 20:51:13 +0100

    My hello world program works

--- Hello/Hello.py
+++ Hello/Hello.py
@@ -1,2 +1,5 @@
 print('This is my first program in pycharm')
 print('Hello world!!!')
+# This is my first program
+print("I am printing a new line")
+print("I prefer Jupyter Notebook rather than Pycharm")
