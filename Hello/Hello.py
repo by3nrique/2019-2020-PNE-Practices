9b389e55746e56879fe63a8c6bcbe1eac5669779
Enrique <e.feito.2019@alumnos.urjc.es>
2020-04-17 23:46:44 +0200

    Hello World!! Final modification

--- Hello/Hello.py
+++ Hello/Hello.py
@@ -1,4 +1,4 @@
-print('This is my first program in pycharm')
-print('Hello world!!!')
+print('This is my first program in pycharm')  # print() function displays information in the screen
+print('Hello world!!!')  # Hello world!!! will appear in the terminal
 # Session 1
 print("I am printing a new line")
